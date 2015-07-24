479328b8c91b31199166f4e2e62a23f7c462a430
sergiolezama <sergio_lezamarv@hotmail.com>
2015-07-24 10:37:38 -0500

    Loader code modifications

--- force_directed.py
+++ force_directed.py
@@ -9,20 +9,7 @@ from jinja2 import BaseLoader, TemplateNotFound, DictLoader
 from jinja2.utils import open_if_exists, internalcode
 from jinja2._compat import string_types, iteritems
 
-class DictLoader(BaseLoader):
     
-    loader = DictLoader({'~/pubmed-mining/force_directed.html': open ( sys.argv[1], 'r')})
-
-    def __init__(self, mapping):
-        self.mapping = mapping
-
-    def get_source(self, environment, template):
-        if template in self.mapping:
-            source = self.mapping[template]
-            return source, None, lambda: source == self.mapping.get(template)
-        raise TemplateNotFound(template)
-
-    def list_templates(self):
-        return sorted(self.mapping)
+loader = DictLoader({'~/pubmed-mining/force_directed.html': open ( sys.argv[1], 'r')})
 
     
